50aba8ad7526c6b08232d0c52381a1c2608b0428
NhanPham03 <ph.nhan03@gmail.com>
2024-08-13 12:06:44 +0700

    Append new pages to routes

--- src/lib/router/routes/private.routes.tsx
+++ src/lib/router/routes/private.routes.tsx
@@ -1,60 +1,67 @@
-import { Suspense } from "react";
+import { lazy, Suspense } from "react";
 import { CustomRouteObject } from "../Router";
+
+const Home = lazy(() => import("@views/Home"));
+
+const ClaimsList = lazy(() => import("@views/claims/ClaimsList"));
+const ClaimsCreate = lazy(() => import("@views/claims/ClaimsCreate"));
+const ClaimsDetails = lazy(() => import("@views/claims/ClaimsDetails"));
+const ClaimsVetting = lazy(() => import("@views/claims/ClaimsVetting"));
 
 const privateRoutes: CustomRouteObject[] = [
   // HOME PAGE
   {
     path: "/",
-    element: <Suspense></Suspense>,
+    element: <Suspense><Home /></Suspense>,
     role: ["admin", "claimer", "approver", "finance"],
   },
   
   // CLAIMS BY STATUS
   {
     path: "/claims/draft",
-    element: <Suspense></Suspense>,
+    element: <Suspense><ClaimsList status="draft" /></Suspense>,
     role: ["admin", "claimer"],
   },
   {
     path: "/claims/pending",
-    element: <Suspense></Suspense>,
+    element: <Suspense><ClaimsList status="pending" /></Suspense>,
     role: ["admin", "claimer", "approver"],
   },
   {
     path: "/claims/approved",
-    element: <Suspense></Suspense>,
+    element: <Suspense><ClaimsList status="approved" /></Suspense>,
     role: ["admin", "claimer", "approver", "finance"],
   },
   {
     path: "/claims/paid",
-    element: <Suspense></Suspense>,
+    element: <Suspense><ClaimsList status="paid" /></Suspense>,
     role: ["admin", "claimer", "finance"],
   },
   {
     path: "/claims/approved-paid",
-    element: <Suspense></Suspense>,
+    element: <Suspense><ClaimsList status={["approved", "paid"]} /></Suspense>,
     role: "approver"
   },
   {
     path: "/claims/rejected-cancelled",
-    element: <Suspense></Suspense>,
+    element: <Suspense><ClaimsList status={["rejected", "cancelled"]} /></Suspense>,
     role: ["admin", "claimer"],
   },
 
   // CLAIM OPERATIONS
   {
     path: "/claims/create",
-    element: <Suspense></Suspense>,
+    element: <Suspense><ClaimsCreate /></Suspense>,
     role: "claimer",
   },
   {
     path: "/claims/:claim_id",
-    element: <Suspense></Suspense>,
+    element: <Suspense><ClaimsDetails /></Suspense>,
     role: ["admin", "claimer", "approver", "finance"],
   },
   {
     path: "/claims/vetting",
-    element: <Suspense></Suspense>,
+    element: <Suspense><ClaimsVetting /></Suspense>,
     role: "approver"
   },
 ];
--- src/lib/router/routes/public.routes.tsx
+++ src/lib/router/routes/public.routes.tsx
@@ -1,18 +1,22 @@
-import { Suspense } from "react";
+import { lazy, Suspense } from "react";
 import { RouteObject } from "react-router-dom";
+
+const Login = lazy(() => import("@views/auth/Login"));
+const Register = lazy(() => import("@views/auth/Register"));
+const ResetPassword = lazy(() => import("@views/auth/ResetPassword"));
 
 const publicRoutes: RouteObject[] = [
   {
     path: "/login",
-    element: <Suspense></Suspense>,
+    element: <Suspense><Login /></Suspense>,
   },
   {
     path: "/register",
-    element: <Suspense></Suspense>,
+    element: <Suspense><Register /></Suspense>,
   },
   {
     path: "/reset-password",
-    element: <Suspense></Suspense>,
+    element: <Suspense><ResetPassword /></Suspense>,
   },
 ];
 
