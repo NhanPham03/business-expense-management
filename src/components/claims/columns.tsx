b4faf943f590c02d081da24b197af10223b63116
NhanPham03 <ph.nhan03@gmail.com>
2024-08-16 21:07:32 +0700

    Scaffolding for cookie utilities & data-table

--- src/components/claims/columns.tsx
+++ src/components/claims/columns.tsx
@@ -3,7 +3,27 @@ import { ColumnDef } from "@tanstack/react-table";
 
 export const columns: ColumnDef<Claim>[] = [
   {
-    accessorKey: "",
-    header: "",
+    accessorKey: "status",
+    header: "Status",
+  },
+  {
+    accessorKey: "staff.name",
+    header: "Staff Name",
+  },
+  {
+    accessorKey: "project.name",
+    header: "Project Name",
+  },
+  {
+    accessorKey: "project.from",
+    header: "From",
+  },
+  {
+    accessorKey: "project.to",
+    header: "To",
+  },
+  {
+    accessorKey: "total_hours",
+    header: "Total Hours",
   },
 ];
