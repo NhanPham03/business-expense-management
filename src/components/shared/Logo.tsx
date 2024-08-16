c2bffeb798b6410b6cb9b0d8aa0e8e0415978a5d
NhanPham03 <ph.nhan03@gmail.com>
2024-08-16 21:03:49 +0700

    Remove template assets & add Logo

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -1,7 +1,8 @@
 export default function Logo() {
   return (
-    <div className="flex h-full justify-center items-center">
-      <img className="h-[50px]" src="/images/vite.svg" alt="AccuRate Logo" />
+    <div className="flex h-full justify-center items-center transition-all">
+      <img className="h-[80%] dark:hidden block" src="/images/Logo_Light.png" alt="AccuRate Lightmode" />
+      <img className="h-[80%] dark:block hidden" src="/images/Logo_Dark.png" alt="AccuRate Darkmode" />
     </div>
   );
 }
